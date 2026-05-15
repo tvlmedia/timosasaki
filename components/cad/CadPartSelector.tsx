3c5d4a50c69b2360e1efe711e14a8c4c3e18b60f
tvlmedia <info@tvlmedia.nl>
2026-05-15 16:37:32 +0200

    Fix CadPartType union for moving carrier and cam sleeve

--- components/cad/CadPartSelector.tsx
+++ components/cad/CadPartSelector.tsx
@@ -10,7 +10,9 @@ export type CadPartType =
   | "retaining_ring"
   | "fixed_pl_barrel_with_slots"
   | "sliding_optical_carrier"
-  | "main_barrel";
+  | "main_barrel"
+  | "moving_carrier"
+  | "cam_sleeve";
 
 const partOptions: Array<{ value: CadPartType; label: string }> = [
   { value: "element_cup", label: "Element cup from glass item" },
